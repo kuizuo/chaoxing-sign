3198d0d93efc4abc1cf5efa54f84f7b2991f19a9
kuizuo <hi@kuizuo.cn>
2023-04-07 01:53:40 +0800

    chore: change logo color

--- components/Hero.vue
+++ components/Hero.vue
@@ -3,9 +3,17 @@
 </script>
 
 <template>
-  <h2 class="font-semibold text-2xl">
-    <Icon name="majesticons:shooting-star-line" size="30" />
-    某星签到
+  <h2 class="font-semibold text-2xl flex justify-center items-center space-x-2">
+    <Icon name="majesticons:shooting-star-line" size="30" color="#e70012" />
+    <n-gradient-text
+      :gradient="{
+        deg: 180,
+        from: '#e70012',
+        to: '#9b1a24',
+      }"
+    >
+      某星签到
+    </n-gradient-text>
   </h2>
   <p class="mt-2">
     本签到助手是一款方便快捷的工具，可以帮助用户记录签到信息。
--- components/app/Logo.vue
+++ components/app/Logo.vue
@@ -7,7 +7,7 @@ const { title, logo } = useAppConfig()
 
 <template>
   <NuxtLink to="/" class="flex items-center justify-center gpa-2">
-    <Icon v-if="/\w+:\w+/.test(logo)" :name="logo" class="w-5 h-5 w-auto" />
+    <Icon v-if="/\w+:\w+/.test(logo)" :name="logo" class="w-5 h-5 w-auto" color="#e70012" />
     <img v-else :src="logo" alt="logo" class="w-5 h-5 w-auto">
     <h2 v-show="showTitle" class="ml-2 truncate md:opacity-100 font-sans leading-1em text-left text-base capitalize">
       {{ title }}
